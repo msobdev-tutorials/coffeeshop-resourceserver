d8a431b4d5b9150429c740805ae80450f36ffdd4
sobolm01 <Michal.Sobolewski@stepstone.com>
2020-05-03 14:02:06 +0200

    add jwt authorization

--- src/main/kotlin/com/okta/kotlin/ResourceServerApplication.kt
+++ src/main/kotlin/com/okta/kotlin/ResourceServerApplication.kt
@@ -10,22 +10,26 @@ class ResourceServerApplication {
 
     @Bean
     fun run(repository: CoffeeRepository) = ApplicationRunner {
-        repository.save(CoffeeShop(
-                name = "Oblique",
-                address = "3039 SE Stark St, Portland, OR 97214",
-                phone = "555111444",
-                priceOfCoffee = 1.50,
-                powerAccessible = true,
-                internetReliability = 5
-        ))
-        repository.save(CoffeeShop(
-                name = "Epoch Coffee",
-                address = "221 W N Loop Blvd, Austin, TX 78751",
-                phone = "555111444",
-                priceOfCoffee = 2.50,
-                powerAccessible = true,
-                internetReliability = 3
-        ))
+        repository.save(
+                CoffeeShop(
+                        name = "Oblique",
+                        address = "3039 SE Stark St, Portland, OR 97214",
+                        phone = "555111444",
+                        priceOfCoffee = 1.50,
+                        powerAccessible = true,
+                        internetReliability = 5
+                )
+        )
+        repository.save(
+                CoffeeShop(
+                        name = "Epoch Coffee",
+                        address = "221 W N Loop Blvd, Austin, TX 78751",
+                        phone = "555111444",
+                        priceOfCoffee = 2.50,
+                        powerAccessible = true,
+                        internetReliability = 3
+                )
+        )
     }
 
 }
